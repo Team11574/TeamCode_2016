247fd8a837427429ecbcff928fcd68fed845de89
Team11574 <Pattinv@gmail.com>
2017-02-25 18:41:14 -0800

    Added Park On Ramp (First Code!)

---
+++ src/main/java/org/firstinspires/ftc/teamcode/Park_on_Ramp.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+/**
+ * Created by FTC Team 11574 on 2/25/2017.
+ *
+ * Drive away from the wall and park on the ramp.
+ */
+
+@Autonomous(name="Park on Ramp", group="Autonomous")
+@SuppressWarnings("unused")
+public class Park_on_Ramp extends Mecanum_Wheels_Generic {
+    @Override
+    public void robotRun() throws InterruptedException {
+        drive_distance(STRAFE_RIGHT, 35.0, 0.6);
+        drive_distance(TURN_LEFT, 10.0, 0.2);
+        drive_distance(DRIVE_FORWARD, 35.0, 0.6);
+    }
+}
+

8c66b8ba28c9486e7bd980178d17d1a4f432a0c2
Sara-Sommer <Tallyho944@gmail.com>
2017-03-04 18:17:54 -0800

    Created a variable to change the direction of the turn.

--- src/main/java/org/firstinspires/ftc/teamcode/Park_on_Ramp.java
+++ src/main/java/org/firstinspires/ftc/teamcode/Park_on_Ramp.java
@@ -14,14 +14,16 @@ public class Park_on_Ramp extends Mecanum_Wheels_Generic {
     @Override
     public void robotRun() {
         AllianceColor color_alliance = check_alliance();
-        int strafe_direction;
+        int strafe_direction, turn_direction;
         if (color_alliance == AllianceColor.Red) {
             strafe_direction = STRAFE_RIGHT;
+            turn_direction = TURN_LEFT;
         } else {
             strafe_direction = STRAFE_LEFT;
+            turn_direction = TURN_RIGHT;
         }
         drive_distance(strafe_direction, 35.0, 0.6);
-        drive_distance(TURN_LEFT, 10.0, 0.2);
+        drive_distance(turn_direction, 10.0, 0.2);
         drive_distance(DRIVE_FORWARD, 35.0, 0.6);
     }
 }
